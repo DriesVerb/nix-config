6e80defcf32cd85098ea509ab64d0217c4a03430
Dries Verboven <verboven.dries@gmail.com>
2024-08-15 12:40:21 +0200

    removing allaises to make them available in bash

--- home-manager/shell.nix
+++ home-manager/shell.nix
@@ -1,5 +1,28 @@
 {
   home.shellAliases = {
+
+    # TODO: integrate nixvim
+    vix = "~/.config/nixvim/result/bin/nvim";
+
+    # shortcut for 
+    evix = "cd ~/.config/nixvim && vix";
+    ehome = "cd ~/&& vix";
+
+    #nix
+    nru = "echo_format 'nix run ' &&  nix run";
+    nbu = "echo_format 'nix build ' &&  nix build";
+    nde = "echo_format 'nix develop ' &&  nix develop";
+
+    #git
+    gaa = "echo_format 'git add . ' &&  git add .";
+    gst = "echo_format 'git status ' && git status";
+    gph = "echo_format 'git push ' && git push";
+    gpl = "echo_format 'git pull ' && git pull";
+    gap = "echo_format 'git add -p ' && git add -p";
+    gcp = "echo_format 'git checkout -p ' && git checkout -p";
+    gcd = "echo_format 'git checkout development ' && git checkout development";
+
+    # home-manager
     hmr = "home-manager switch --flake .#eta@popos";
   };
 }
