6c02d182bd67dfe40b32cba276724967d506a6eb
Dries Verboven <verboven.dries@gmail.com>
2024-08-15 19:32:16 +0200

    adding new config

--- home-manager/bash.nix
+++ home-manager/bash.nix
@@ -3,26 +3,31 @@
     enable = true;
 
     initExtra = ''
-                    BLACK="\e[30m"
-                    BOLD="\e[1m"
-                    NORMAL="\e[0m"
-                    BLUE_B="\e[104m"
-                    END="\e[0m"
+                                BLACK="\e[30m"
+                                BOLD="\e[1m"
+                                NORMAL="\e[0m"
+                                BLUE_B="\e[104m"
+                                END="\e[0m"
 
-      echo_format () {
-      echo -e "$BLACK$BLUE_B     $BOLD$1   $NORMAL$BLACK$BLUE_B󱜙 $END"
-                    }
+                  echo_format () {
+                  echo -e "$BLACK$BLUE_B     $BOLD$1   $NORMAL$BLACK$BLUE_B󱜙 $END"
+                                }
+            #Adding branch name to bash
+            parse_git_branch() {
+                 git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+            }
 
-      alias gcm='f() {
-      echo_format "git commit -m '$1'"
-      git commit -m "$1" &&
-      };f'
+      export PS1="$_CYAN $_GREEN@$_BLUE \w$_PURPLE#$_ORANGE\$(parse_git_branch) $_RESET"
+                  alias gcm='f() {
+                  echo_format "git commit -m '$1'"
+                  git commit -m "$1" &&
+                  };f'
       
-      alias gnb='f() {
-      git branch "$1" &&
-      git checkout "$1" &&
-      echo_format "git branch $1 + git checkout $1"
-      };f'
+                  alias gnb='f() {
+                  git branch "$1" &&
+                  git checkout "$1" &&
+                  echo_format "git branch $1 + git checkout $1"
+                  };f'
 
     '';
   };
