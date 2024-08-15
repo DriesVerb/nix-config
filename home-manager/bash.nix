00ad51d52aacf7aee0ae7c7511bb216ee3906a47
Dries Verboven <verboven.dries@gmail.com>
2024-08-15 13:28:52 +0200

    adding function to bash

--- home-manager/bash.nix
+++ home-manager/bash.nix
@@ -1,5 +1,29 @@
 {
   programs.bash = {
     enable = true;
+
+    initExtra = ''
+                    BLACK="\e[30m"
+                    BOLD="\e[1m"
+                    NORMAL="\e[0m"
+                    BLUE_B="\e[104m"
+                    END="\e[0m"
+
+      echo_format () {
+      echo -e "$BLACK$BLUE_B     $BOLD$1   $NORMAL$BLACK$BLUE_B󱜙 $END"
+                    }
+
+      alias gcm='f() {
+      echo_format "git commit -m '$1'"
+      git commit -m "$1" &&
+      };f'
+      
+      alias gnb='f() {
+      git branch "$1" &&
+      git checkout "$1" &&
+      echo_format "git branch $1 + git checkout $1"
+      };f'
+
+    '';
   };
 }
