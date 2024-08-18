a536a4846fbd94067292e76d3b1b90ced7182604
Dries Verboven <verboven.dries@gmail.com>
2024-08-18 19:45:46 +0200

    finding typo from function

--- home-manager/bash.nix
+++ home-manager/bash.nix
@@ -22,18 +22,18 @@
                                                    END="\e[0m"
 
                                      echo_format () {
-                                     echo -e "$BLACK$BLUE_B     $BOLD$1   $NORMAL$BLACK$BLUE_B󱜙 $END"
-                                                   }
+                                     echo -e "$BLACK$BLUE_B     $BOLD$1   $NORMAL$BLACK$BLUE_B󱜙 $END"}
                                #Adding branch name to bash
                                parse_git_branch() {
                                     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
                                }
 
                          export PS1="$_CYAN $_GREEN@$_BLUE \w$_PURPLE#$_ORANGE\$(parse_git_branch) $_RESET"
-                                     alias gcm='f() {
-                                     echo_format "git commit -m '$1'"
-                                     git commit -m "$1" &&
-                                     };f'
+
+            alias gcm='f() {
+            echo_format "git commit -m '$1'"
+            git commit -m "$1" &&
+            };f'
       
              alias gnb='f() {
              git branch "$1" &&
