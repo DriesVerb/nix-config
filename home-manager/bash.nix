d3859ced7a6819f3132a5e0c09c80f6fed4cfc5e
Dries Verboven <verboven.dries@gmail.com>
2024-08-18 20:14:41 +0200

    adding copied shortcusr

--- home-manager/bash.nix
+++ home-manager/bash.nix
@@ -8,6 +8,32 @@
       };
 
     initExtra = ''
+                          BLACK="\e[30m"
+                          BOLD="\e[1m"
+                          NORMAL="\e[0m"
+                          BLUE_B="\e[104m"
+                          END="\e[0m"
+
+            echo_format () {
+            echo -e "$BLACK$BLUE_B     $BOLD$1   $NORMAL$BLACK$BLUE_B󱜙 $END"
+                          }
+
+            alias gcm='f() {
+            echo_format "git commit -m '$1'"
+            git commit -m "$1" &&
+            };f'
+      
+            alias gnb='f() {
+            git branch "$1" &&
+            git checkout "$1" &&
+            echo_format "git branch $1 + git checkout $1"
+            };f'
+
+      # Tmux shortcuts
+      alias tmr='f() {
+      tmux rename-window $1
+      };f'
+
                    _GREEN="\[$(tput setaf 046)\]"
                    _BLUE="\[$(tput setaf 045)\]"
                    _CYAN="\[$(tput setaf 014)\]"
@@ -15,15 +41,7 @@
                    _PURPLE="\[$(tput setaf 135)\]"
                    _RESET="\[$(tput sgr0)\]"
                    _BOLD=$(tput bold)
-                                                         BLACK="\e[30m"
-                                                         BOLD="\e[1m"
-                                                         NORMAL="\e[0m"
-                                                         BLUE_B="\e[104m"
-                                                         END="\e[0m"
 
-                                           echo_format () {
-                                           echo -e "$BLACK$BLUE_B     $BOLD$1   $NORMAL$BLACK$BLUE_B󱜙 $END"
-      }
                                      #Adding branch name to bash
                                      parse_git_branch() {
                                           git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
